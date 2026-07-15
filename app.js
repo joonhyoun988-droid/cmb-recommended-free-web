0922a85f4c44b1a0c65ffdb0552e7883b97aab92
joonhyoun988-droid <225554512+joonhyoun988-droid@users.noreply.github.com>
2026-07-15 22:43:19 +0900

    fix: include Google session in CMB API requests

--- app.js
+++ app.js
@@ -954,6 +954,7 @@ function postEndpoint(action, payload, endpointOverride) {
   if (!endpoint) return Promise.reject(new Error("Endpoint is not configured"));
   return fetch(endpoint, {
     method: "POST",
+    credentials: "include",
     headers: { "content-type": "application/json" },
     body: JSON.stringify(Object.assign({ action }, payload || {}))
   }).then((response) => {
